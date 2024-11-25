b509b6662cd010daa41f4e54ad2023c8c985fa73
George Salukvadze <gsalukvadze@iotlab.com>
2024-11-25 14:41:18 +0100

    - add Node 22

---
+++ node_22/tmp/nodeyarn.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+mkdir /home/root
+
+touch ~/.bashrc
+
+rm -rf /usr/share/man /var/cache/apk/* \
+  /root/.npm /root/.node-gyp /root/.gnupg /usr/lib/node_modules/npm/man \
+  /usr/lib/node_modules/npm/doc /usr/lib/node_modules/npm/html /usr/lib/node_modules/npm/scripts
