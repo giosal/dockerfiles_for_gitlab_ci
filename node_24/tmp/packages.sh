4fde5db7f4db955458fbadcb3d0b29fec8d78df1
George Salukvadze <gsalukvadze@iotlab.com>
2026-06-15 15:11:11 +0200

    - add a version for PHP 8.5 and Node 26

---
+++ node_24/tmp/packages.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+apk --update --no-cache add \
+    g++ \
+    gcc \
+    git \
+    grep \
+    jq \
+    libc-dev \
+    make \
+    nodejs \
+    npm \
+    openssh-client \
+    openssl \
+    patch \
+    python3 \
+    rsync \
+    sudo \
+    zip
+
+# persistent / runtime deps
+apk add --update --no-cache --virtual .persistent-deps \
+		ca-certificates \
+		tar \
+		xz \
+    curl
+
+apk add --update --no-cache \
+    autoconf \
+    build-base \
+    file \
+    openssl-dev
